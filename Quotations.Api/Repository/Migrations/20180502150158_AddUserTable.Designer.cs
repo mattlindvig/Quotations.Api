115354ef07b9ece5783c18b4e16f7c965c9191b4
mattlindvig <mattlindvig@gmail.com>
2018-05-02 13:40:00 -0500

    now works with docker!

--- Quotations.Api/Repository/Migrations/20180502150158_AddUserTable.Designer.cs
+++ Quotations.Api/Repository/Migrations/20180502150158_AddUserTable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Repository.Migrations
 {
     [DbContext(typeof(DatabaseSetup))]
-    partial class DatabaseThingModelSnapshot : ModelSnapshot
+    [Migration("20180502150158_AddUserTable")]
+    partial class AddUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Quotations.Api/Repository/Migrations/20180502150158_AddUserTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Repository.Migrations
+{
+    public partial class AddUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Quotes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsersUserId",
+                table: "Quotes",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_UsersUserId",
+                table: "Quotes",
+                column: "UsersUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quotes_User_UsersUserId",
+                table: "Quotes",
+                column: "UsersUserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quotes_User_UsersUserId",
+                table: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Quotes_UsersUserId",
+                table: "Quotes");
+
+            migrationBuilder.DropColumn(
+                name: "UsersUserId",
+                table: "Quotes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Quotes",
+                nullable: true);
+        }
+    }
+}
